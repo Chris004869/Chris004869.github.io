1fbe9438cc2979b895ad68d1f789392d5e8e6da7
Chris <gaoyuan20041022@126.com>
2025-08-25 20:14:06 +0800

    Site updated: 2025-08-25 20:14:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/25/hello-world/","2025/08/25/第一篇博客/","2025/08/25/index/"];function toRandomPost(){
+var posts=["2025/08/25/hello-world/","2025/08/25/index/","2025/08/25/第一篇博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
