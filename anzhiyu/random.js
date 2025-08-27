3de3255edcc43d85e1358c475b59f5251bcedf3d
Chris <gaoyuan20041022@126.com>
2025-08-27 23:46:38 +0800

    Site updated: 2025-08-27 23:46:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/25/hello-world/","2025/08/25/index/","2025/08/25/第一篇博客/"];function toRandomPost(){
+var posts=["2025/08/27/政策学习总结：国务院关于深入实施“人工智能-”行动的意见/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
